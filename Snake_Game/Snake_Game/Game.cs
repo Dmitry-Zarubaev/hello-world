93b2ab4cf6cb4364f4b81485bfdbcea12234a415
Dmitry-Zarubaev <dmitry.zarubaev@area9.dk>
2020-02-11 19:09:56 +0300

    Lesson 2: Procedure approach (an example)

---
+++ Snake_Game/Snake_Game/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake {
+    class Game {
+        static void DrawChar(int x, int y, char symbol) {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+        static void Main(string[] args) {
+            DrawChar(1, 3, '*');
+            DrawChar(4, 5, '#');
+
+            Console.ReadLine();
+        }
+    }
+}
